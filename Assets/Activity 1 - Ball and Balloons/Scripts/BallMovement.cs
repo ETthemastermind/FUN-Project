b4f0b7912a0d4c8600b846c1c2df5e159ab8e111
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-09-02 15:51:00 +0100

    Ball is limited (albeit hard coded ish) to the "grid" and returns a console message when it hits the wall. working on the rotation of the ball now

--- Assets/Activity 1 - Ball and Balloons/Scripts/BallMovement.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/BallMovement.cs
@@ -10,9 +10,12 @@ public class BallMovement : MonoBehaviour
 
     public Vector3 BallStart;
     public Vector3 BallDestination;
+    public bool ForwardDirection;
+    public bool SidewaysDirection;
 
     public float LerpFraction;
     public float LerpSpeed = 0.5f;
+    public float RotSpeed = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,8 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (Input.GetKeyDown(KeyCode.UpArrow))  //debug arrow key movement
         {
             BallForwards();
@@ -55,12 +60,24 @@ public class BallMovement : MonoBehaviour
             {
                 LerpFraction += Time.deltaTime * LerpSpeed;
                 gameObject.transform.position = Vector3.Lerp(BallStart, BallDestination, LerpFraction);
+
+                //gameObject.transform.Rotate(0, 0, RotSpeed * Time.deltaTime);
+                if (ForwardDirection == true)
+                {
+                    gameObject.transform.Rotate(0, 0, RotSpeed * Time.deltaTime);
+                }
+                else if (ForwardDirection == false)
+                {
+                    gameObject.transform.Rotate(0, 0, (RotSpeed * -1) * Time.deltaTime);
+                }
+               
             }
 
             else
             {
                 _BallIsMoving = false;
                 LerpFraction = 0f;
+                
             }
 
 
@@ -76,7 +93,19 @@ public class BallMovement : MonoBehaviour
             Debug.Log("Ball forwards button press active");
             BallStart = gameObject.transform.position;
             BallDestination = new Vector3(BallStart.x + MoveValue, gameObject.transform.position.y, gameObject.transform.position.z); //move ball right by the increment value
-            _BallIsMoving = true;
+            if (BallStart.x != 5)
+            {
+                _BallIsMoving = true;
+
+                ForwardDirection = true;
+
+            }
+            else
+            {
+                Debug.Log("Ball at the end of the grid");
+            }
+            
+            
         }
         
     }
@@ -89,6 +118,7 @@ public class BallMovement : MonoBehaviour
             BallStart = gameObject.transform.position;
             BallDestination = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, BallStart.z - MoveValue); //move ball right by the increment value
             _BallIsMoving = true;
+            SidewaysDirection = true;
 
         }
         
@@ -103,6 +133,7 @@ public class BallMovement : MonoBehaviour
             BallStart = gameObject.transform.position;
             BallDestination = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, BallStart.z + MoveValue); //move ball right by the increment value
             _BallIsMoving = true;
+            
 
         }
         
@@ -119,7 +150,20 @@ public class BallMovement : MonoBehaviour
             BallDestination = new Vector3(BallStart.x - MoveValue, gameObject.transform.position.y, gameObject.transform.position.z); //move ball right by the increment value
             _BallIsMoving = true;
 
+            ForwardDirection = false;
+
         }
         
     }
+    
+    
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Boundary")
+        {
+            Debug.Log("Bzz Bzz Haptic Feedback Bzz Bzz");
+        }
+    }
+
+    
 }
