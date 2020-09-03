8ba226317147a2d13a0e7d1721e628df626fd69e
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-09-03 17:56:43 +0100

    added runtime scaling to the runtime button move

--- Assets/RuntimeButtonMove.cs
+++ Assets/RuntimeButtonMove.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class RuntimeButtonMove : MonoBehaviour
 {
     public bool ElementPickedUp;
-    public Vector3 Location;
+    
+    public Vector2 CurrentMouseLocation;
 
+
+    //a series of arrays keeping track of what the anchor values are
     private float[] Anchor_TopLeft = new float[2] {0,1};
     private float[] Anchor_TopMid = new float[2] {0.5f, 1};
     private float[] Anchor_TopRight = new float[2] {1,1};
@@ -31,21 +34,41 @@ public class RuntimeButtonMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CurrentMouseLocation = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
         if (ElementPickedUp == true)
         {
             //Debug.Log(Input.mousePosition);
-            Location = Input.mousePosition; //gets the current location 
-            gameObject.transform.position = Location;
-            
+
+            gameObject.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);            
         }
 
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            this.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(Anchor_BottomLeft[0], Anchor_BottomLeft[1]);
-            this.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(Anchor_BottomLeft[0], Anchor_BottomLeft[1]);
+            this.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(Anchor_TopRight[0], Anchor_TopRight[1]);
+            this.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(Anchor_TopRight[0], Anchor_TopRight[1]);
         }
-        
+
+        //scaling testing ========================================================================================
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            Debug.Log("Scale Up");
+            float NewY = gameObject.transform.localScale.y + 0.1f;
+            float NewX = gameObject.transform.localScale.x + 0.1f;
+            gameObject.transform.localScale = new Vector3(NewX, NewY, transform.localScale.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            Debug.Log("Scale Down");
+            float NewY = gameObject.transform.localScale.y - 0.1f;
+            float NewX = gameObject.transform.localScale.x - 0.1f;
+            gameObject.transform.localScale = new Vector3(NewX, NewY, transform.localScale.z);
+        }
+
+       
+        //========================================================================================================
 
     }
 
