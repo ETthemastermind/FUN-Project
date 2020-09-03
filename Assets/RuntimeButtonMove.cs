d4532e31fa22a8f52017240c11749698ee1f8d6e
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-09-03 17:15:07 +0100

    worked a bit more of changing UI at runtime, changing anchors in theory works but hasnt been implemented 100% yet

--- Assets/RuntimeButtonMove.cs
+++ Assets/RuntimeButtonMove.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RuntimeButtonMove : MonoBehaviour
 {
     public bool ElementPickedUp;
     public Vector3 Location;
+
+    private float[] Anchor_TopLeft = new float[2] {0,1};
+    private float[] Anchor_TopMid = new float[2] {0.5f, 1};
+    private float[] Anchor_TopRight = new float[2] {1,1};
+
+    private float[] Anchor_MidLeft = new float[2] {0,0.5f};
+    private float[] Anchor_MidMid = new float[2] {0.5f, 0.5f };
+    private float[] Anchor_MidRight = new float[2] { 1, 0.5f };
+
+    private float[] Anchor_BottomLeft = new float[2] {0,0};
+    private float[] Anchor_BottomMid = new float[2] { 0.5f, 0 };
+    private float[] Anchor_BottomRight = new float[2] { 1 , 0 };
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,11 +33,20 @@ public class RuntimeButtonMove : MonoBehaviour
     {
         if (ElementPickedUp == true)
         {
-            Debug.Log(Input.mousePosition);
-            Location = Input.mousePosition;
+            //Debug.Log(Input.mousePosition);
+            Location = Input.mousePosition; //gets the current location 
             gameObject.transform.position = Location;
+            
         }
-       
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(Anchor_BottomLeft[0], Anchor_BottomLeft[1]);
+            this.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(Anchor_BottomLeft[0], Anchor_BottomLeft[1]);
+        }
+        
+
     }
 
     public void PickUp()
@@ -35,11 +60,14 @@ public class RuntimeButtonMove : MonoBehaviour
 
         else if (ElementPickedUp == true)
         {
+            
             ElementPickedUp = false;
-            Debug.Log("Element Put Down");
+            
         }
 
 
 
     }
+
+    
 }
