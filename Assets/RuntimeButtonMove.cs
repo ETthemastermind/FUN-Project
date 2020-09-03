98d9dbc36159a45b10c97313c44dc9c7261967cf
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-09-03 16:33:16 +0100

    Experimenting with moving UI at runtime

---
+++ Assets/RuntimeButtonMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeButtonMove : MonoBehaviour
+{
+    public bool ElementPickedUp;
+    public Vector3 Location;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ElementPickedUp == true)
+        {
+            Debug.Log(Input.mousePosition);
+            Location = Input.mousePosition;
+            gameObject.transform.position = Location;
+        }
+       
+    }
+
+    public void PickUp()
+    {
+        if (ElementPickedUp == false)
+        {
+            ElementPickedUp = true;
+            Debug.Log("Element Picked Up");
+
+        }
+
+        else if (ElementPickedUp == true)
+        {
+            ElementPickedUp = false;
+            Debug.Log("Element Put Down");
+        }
+
+
+
+    }
+}
