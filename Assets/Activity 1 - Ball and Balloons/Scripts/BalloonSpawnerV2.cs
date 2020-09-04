a68147d73138efd5f89aa7c1f1ae8a8e83c8e2f3
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-09-04 18:45:41 +0100

    Final commit for the day, replaced the default ball and balloons with properly textured models. Balloon colors are selected at random.code shoud be properly commented. added some sound effects and a read me.

--- Assets/Activity 1 - Ball and Balloons/Scripts/BalloonSpawnerV2.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/BalloonSpawnerV2.cs
@@ -5,17 +5,19 @@ using System.Linq;
 
 public class BalloonSpawnerV2 : MonoBehaviour
 {
-    public GameObject Ball_Player;
-    public GameObject BalloonPrefab;
-    public int NumberOfBalloonsToSpawn;
-    public new List<Vector3> SpawnLocations = new List<Vector3>();
-    public GameObject[] SpawnedBalloons;
-    public GameObject HUDController;
+    public GameObject Ball_Player; //reference to the player
+    public GameObject BalloonPrefab; //reference to the balloon prefab 
+    public int NumberOfBalloonsToSpawn; // Reference to number of balloons to spawn per round
+    public new List<Vector3> SpawnLocations = new List<Vector3>(); //list of the spawn locations for the balloons, randomly generated
+    public GameObject[] SpawnedBalloons; //array for all balloons in the scene
+    public GameObject HUDController; //reference to the HUD controller
+
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        Ball_Player = GameObject.FindGameObjectWithTag("Player");
+        Ball_Player = GameObject.FindGameObjectWithTag("Player"); //find the player
         
 
     }
@@ -23,34 +25,34 @@ public class BalloonSpawnerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpawnedBalloons = GameObject.FindGameObjectsWithTag("Balloon");
-        if (SpawnedBalloons.Length == 0)
+        SpawnedBalloons = GameObject.FindGameObjectsWithTag("Balloon"); //finds all the ballons active in the scene per frame
+        if (SpawnedBalloons.Length == 0) //if there are no balloons in the scene
         {
-            if (HUDController.GetComponent<HudController>().GameComplete == false)
+            if (HUDController.GetComponent<HudController>().GameComplete == false) //checks the HUDController to make sure that the player hasnt reached the max score (had an issue of balloons spawning and then the time scale turning to 0)
             {
-                SpawnBalloons();
+                SpawnBalloons(); //run the spawn balloon function
             }
             
 
         }
     }
-    public void SpawnBalloons()
+    public void SpawnBalloons() //function to spawn balloons
     {
 
-        SpawnLocations.Clear();
-        for (int i = 0; i <= NumberOfBalloonsToSpawn - 1; i++)
+        SpawnLocations.Clear(); //clears the current list of spawn locations for balloons
+        for (int i = 0; i <= NumberOfBalloonsToSpawn - 1; i++) //for the number of balloons to spawn (-1 is there so that the chosen number of balloons set in the inspector works with the list)
         {
 
-            Vector3 SpawnLocation = new Vector3(Random.Range(-5, 5), transform.position.y, Random.Range(-5, 5));
-            SpawnLocations.Add(SpawnLocation);
+            Vector3 SpawnLocation = new Vector3(Random.Range(-5, 5), transform.position.y, Random.Range(-5, 5)); //generate a random location for a balloon to spawn
+            SpawnLocations.Add(SpawnLocation); //adds the random location to the list of locations
 
         }
 
-        CheckForDuplicates();
+        CheckForDuplicates(); //run the check for duplicates function
 
-        for (int j = 0; j != SpawnLocations.Count; j++)
+        for (int j = 0; j != SpawnLocations.Count; j++) //for the length of the spawn locations list (basically how many balloons are wanted to spawn)
         {
-            Instantiate(BalloonPrefab, SpawnLocations[j], Quaternion.identity);
+            Instantiate(BalloonPrefab, SpawnLocations[j], Quaternion.identity); //spawn a balloon at the location held in element J of the list
             
 
         }
@@ -60,15 +62,15 @@ public class BalloonSpawnerV2 : MonoBehaviour
         
 
     }
-    public void CheckForDuplicates()
+    public void CheckForDuplicates() //function to check for duplicates in the list of locations, I got unlucky and had 3 out of 4 balloons spawn in the same place
     {
-        SpawnLocations = SpawnLocations.Distinct().ToList();
-        if (SpawnLocations.Count <= 3)
+        SpawnLocations = SpawnLocations.Distinct().ToList(); //removes all duplicates in the list
+        if (SpawnLocations.Count <= 3) //if the length of the list is less than 3
         {
-            Debug.Log("Duplicate Spawn Location Found");
-            Vector3 SpawnLocation = new Vector3(Random.Range(-5, 5), transform.position.y, Random.Range(-5, 5));
-            SpawnLocations.Add(SpawnLocation);
-            CheckForDuplicates();
+            Debug.Log("Duplicate Spawn Location Found"); //print that a duplicate spawn location was generated to the console
+            Vector3 SpawnLocation = new Vector3(Random.Range(-5, 5), transform.position.y, Random.Range(-5, 5)); //create a new location to replace the duplicated
+            SpawnLocations.Add(SpawnLocation); //adds the replacement location to the list
+            CheckForDuplicates(); //run the check duplicates function again, essentially creating a loop until 4 unique locations are generated
         }
         
 
--- Assets/Activity 1 - Ball and Balloons/Scripts/Balloons.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/Balloons.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class Balloons : MonoBehaviour
 {
-    //public float BalloonSpeed = 10f;  //public float for the speed of the balloon
-    public float BalloonRotSpeed = 10f;
+    
+    public float BalloonRotSpeed = 10f; //value for the rotation speed of the balloon
+
+    public Texture[] BalloonColors; //array of albedo maps for the balloons
+    public GameObject Camera; //reference to the camera because it has an audio source
+    public AudioSource AS; //reference to the audio source on the camera
+    public AudioClip Pop_SFX; //pop sound effect for the balloon
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Balloon Spawned"); //debug to confirm that the balloon has spawned
+        int RandomColor = Random.Range(0, 5); //pick a number at random
+        //Debug.Log(RandomColor);
+        Material BalloonMat = gameObject.GetComponent<Renderer>().material; //gets the material on the balloon
+        BalloonMat.SetTexture("_MainTex", BalloonColors[RandomColor]); //change the albedo map of the balloon to the randomly chosen one
+
+        gameObject.transform.Rotate(-90f, 0f,0f); //damn balloon wont spawn in the same orientation that dragging it into the scene shows
+
+        Camera = GameObject.FindGameObjectWithTag("MainCamera"); //finds the main camera
+        AS = Camera.GetComponent<AudioSource>(); //gets the audiosource on the camera object
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(0, BalloonRotSpeed, 0);
+        gameObject.transform.Rotate(0,0,BalloonRotSpeed); //rotate the balloon around the Z axis, doesnt look as good because the texture is uniform across the whole model but will look better with texture variation i.e numbers
         //gameObject.transform.position += Vector3.left * BalloonSpeed * Time.deltaTime; //move the balloon left at it's speed * time.delta time (left happens to be down towards the player here)
     }
 
-
-    /*
-    public void OnTriggerEnter(Collider other)
+    public void OnDestroy()
     {
-        if (other.gameObject.tag == "BalloonDestory") //if the balloon leaves the player's view and enters the trigger of the mesh behind them 
-        {
-            Debug.Log("Balloon Missed"); //print to console that the player has missed the balloon
-            Destroy(gameObject); //destroy the balloon
-        }
+        AS.PlayOneShot(Pop_SFX); //when the ballon is destroyed, play the pop sound effect
     }
-    */
+
+
+
+
+
 }
