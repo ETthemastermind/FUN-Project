ec629ce9a2788edf25dd996dbdb6e7b93b208396
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-14 13:08:35 +0100

    reworked UI so that there arent any unneccessary duplications

--- Assets/Activity 1 - Ball and Balloons/Scripts/CameraMovementV2.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/CameraMovementV2.cs
@@ -7,6 +7,12 @@ public class CameraMovementV2 : MonoBehaviour
     public Transform[] CameraPositionsArray; //array of camera positions
     public GameObject[] CameraInterface_Canvas; //array of canvases, canvas 0 is for camera 0 etc
     public int CurrentCamera = 0; //ref to the current active camera
+
+    public Quaternion TargetRot;    
+    public Quaternion StartRotation;
+
+    public float LerpSpeed = 1f;
+    public float LerpFraction;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +40,8 @@ public class CameraMovementV2 : MonoBehaviour
         transform.parent = CameraPositionsArray[CurrentCamera].transform; //set the camera to a be a child of the position game object and sets its transforms too
         transform.position = CameraPositionsArray[CurrentCamera].transform.position;
         transform.rotation = CameraPositionsArray[CurrentCamera].transform.rotation;
+
+
         /*
         StartRotation = transform.rotation.eulerAngles;
         */
@@ -42,6 +50,7 @@ public class CameraMovementV2 : MonoBehaviour
     public void RotateCameraUp()
     {
         CameraPositionsArray[CurrentCamera].GetComponent<CameraPosMovement>().RotateUp();
+
     }
     public void RotateCameraRight()
     {
@@ -54,5 +63,19 @@ public class CameraMovementV2 : MonoBehaviour
     public void RotateCameraLeft()
     {
         CameraPositionsArray[CurrentCamera].GetComponent<CameraPosMovement>().RotateLeft();
+    }
+
+    public IEnumerator RotCam(Quaternion RotTarget)
+    {
+        while (LerpFraction < 1)
+        {
+            Debug.Log("Rotating Camera Up");
+            yield return new WaitForEndOfFrame();
+            LerpFraction += LerpSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(StartRotation, RotTarget, LerpFraction);
+        }
+        LerpFraction = 0f;
+        //StartCoroutine(ReturnCamera(RotTarget));
+
     }
 }
--- Assets/Activity 1 - Ball and Balloons/Scripts/CameraPosMovement.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/CameraPosMovement.cs
@@ -12,6 +12,9 @@ public class CameraPosMovement : MonoBehaviour
     public float TimeBeforeReturn; //how long the camera gets hold in rotation for
     public float CurrentTimeBeforeReturn; //how long the user has already waited for
 
+    public float UpDownRotAngle;
+    public float LeftRightRotAngle;
+
     void Start()
     {
         TargetRot = transform.rotation;
@@ -22,35 +25,7 @@ public class CameraPosMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log("Space Pressed");
-            TargetRot *= Quaternion.AngleAxis(20, Vector3.up);
-        }
-        else if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log("Space Pressed");
-            TargetRot *= Quaternion.AngleAxis(20, Vector3.down);
-        }
-
-        else if (Input.GetKeyDown(KeyCode.H))
-        {
-            Debug.Log("Space Pressed");
-            TargetRot *= Quaternion.AngleAxis(20, Vector3.right);
-        }
-
-        else if (Input.GetKeyDown(KeyCode.F))
-        {
-            Debug.Log("Space Pressed");
-            TargetRot *= Quaternion.AngleAxis(20, Vector3.left);
-        }
         
-         
-        
-        float t = Mathf.Clamp(10 * Smooth * Time.deltaTime, 0f, 0.99f);
-        transform.rotation = Quaternion.Lerp(transform.rotation, TargetRot, t);
-        */
     }
 
     public void DeclarePosition()
@@ -60,25 +35,25 @@ public class CameraPosMovement : MonoBehaviour
 
     public void RotateUp()
     {
-        TargetRot *= Quaternion.AngleAxis(20, Vector3.left);
+        TargetRot *= Quaternion.AngleAxis(UpDownRotAngle, Vector3.left);
         StartCoroutine(RotCam(TargetRot));
         TargetRot = StartRotation;
     }
     public void RotateRight()
     {
-        TargetRot *= Quaternion.AngleAxis(20, Vector3.up);
+        TargetRot *= Quaternion.AngleAxis(LeftRightRotAngle, Vector3.up);
         StartCoroutine(RotCam(TargetRot));
         TargetRot = StartRotation;
     }
     public void RotateDown()
     {
-        TargetRot *= Quaternion.AngleAxis(20, Vector3.right);
+        TargetRot *= Quaternion.AngleAxis(UpDownRotAngle, Vector3.right);
         StartCoroutine(RotCam(TargetRot));
         TargetRot = StartRotation;
     }
     public void RotateLeft()
     {
-        TargetRot *= Quaternion.AngleAxis(20, Vector3.down);
+        TargetRot *= Quaternion.AngleAxis(LeftRightRotAngle, Vector3.down);
         StartCoroutine(RotCam(TargetRot));
         TargetRot = StartRotation;
     }
@@ -98,7 +73,7 @@ public class CameraPosMovement : MonoBehaviour
         StartCoroutine(ReturnCamera(RotTarget));
 
     }
-
+    #region Rotate the Camera // "It's over, Anakin. I have the high ground"
     public IEnumerator ReturnCamera(Quaternion RotTarget) //return the camera 
     {
         yield return StartCoroutine(RotPause()); //start the coroutine to hold the camera in place
@@ -129,21 +104,7 @@ public class CameraPosMovement : MonoBehaviour
         LerpFraction = 0f;
     }
 
-    /*
-     * public IEnumerator RotCamStart(Vector3 RotTarget) //basically the same as the RotCam ienum, just in reverse
-    {
-        //RotationTarget = new Vector3(StartRotation.x - MaxRot, StartRotation.y, StartRotation.z);
-        while (LerpFraction < 1)
-        {
-            Debug.Log("Rotating Camera to Start");
-            yield return new WaitForEndOfFrame();
-            LerpFraction += LerpSpeed * Time.deltaTime;
-            gameObject.transform.eulerAngles = Vector3.Lerp(RotTarget, StartRotation, LerpFraction);
-        }
-        LerpFraction = 0f;
-        _CameraMoving = false; //turn the camera moving varibale back off;
-    }
-     
-    
-    */
+    #endregion
+
+
 }
