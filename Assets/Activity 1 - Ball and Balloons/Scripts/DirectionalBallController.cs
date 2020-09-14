7e6c8eae4a177c4375f7296c496f5f032bcbe0e9
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-14 17:58:19 +0100

    Final  commit, only change between this and last is i have commented the directional ball controller

---
+++ Assets/Activity 1 - Ball and Balloons/Scripts/DirectionalBallController.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalBallController : MonoBehaviour
+{
+    public float LerpFraction; //public float for keeping track of the lerp fraction
+    public float RotSpeed; //rotation speed 
+    public float TravelSpeed; //travel speed
+    public float TravelDistance = 1f; //distance the ball travels
+
+    public bool _BallMoving = false; //bool to keep track if the ball is moving
+    //public Vector3 Target;
+    //public Vector3 StartRot;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.localPosition);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //debug controls
+        {
+            RotateForward();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            RotateBackwards();
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RotateRight();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            RotateLeft();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            MoveBall();
+        }
+    }
+
+    public void MoveBall() //fucntion to move the ball
+    { 
+        Vector3 StartPos = transform.localPosition; //gets the current local position of the ball
+        Vector3 Target = transform.localPosition + (transform.right * TravelDistance); //calculates the destination of the ball
+        StartCoroutine(MoveTowards(StartPos, Target)); //run the coroutine to move the ball
+    }
+    public void RotateForward() //functions to set the ball to rotate to face the chosen direction, 
+    {
+        if (_BallMoving == false) //if the ball is not moving
+        {
+            _BallMoving = true; //turn the bool to true, therefore the ball is moving
+            Debug.Log("Rotate to forward"); //debug which way the ball is rotating to the console
+            Vector3 StartRot = transform.eulerAngles; //gets the current rotation
+            Vector3 Target = new Vector3(transform.eulerAngles.x, 0f, transform.eulerAngles.z); //calculates the target rotation
+            StartCoroutine(RotateTowardsDirection(Target, StartRot)); //runs the coroutine to rotate the ball
+            Debug.Log("Rotation Complete"); //debug to console that the rotation is complete
+        }
+        
+    }
+
+    public void RotateBackwards() //same as above but backwards
+    {
+        if (_BallMoving == false)
+        {
+            _BallMoving = true;
+            Debug.Log("Rotate to backwards");
+            Vector3 StartRot = transform.eulerAngles;
+            Vector3 Target = new Vector3(transform.eulerAngles.x, 180f, transform.eulerAngles.z);
+            StartCoroutine(RotateTowardsDirection(Target, StartRot));
+            Debug.Log("Rotation Complete");
+        }
+        
+    }
+    
+    public void RotateRight() //same as above but right
+    {
+        if (_BallMoving == false)
+        {
+            _BallMoving = true;
+            Debug.Log("Rotate to right");
+            Vector3 StartRot = transform.eulerAngles;
+            Vector3 Target = new Vector3(transform.eulerAngles.x, 90f, transform.eulerAngles.z);
+            StartCoroutine(RotateTowardsDirection(Target, StartRot));
+            Debug.Log("Rotation Complete");
+        }
+        
+    }
+
+    public void RotateLeft() //same as above but left
+    {
+        if (_BallMoving == false)
+        {
+            _BallMoving = true;
+            Debug.Log("Rotate to left");
+            Vector3 StartRot = transform.eulerAngles;
+            Vector3 Target = new Vector3(transform.eulerAngles.x, 270f, transform.eulerAngles.z);
+            StartCoroutine(RotateTowardsDirection(Target, StartRot));
+            Debug.Log("Rotation Complete");
+
+        }
+        
+
+    }
+    
+    
+    
+    
+    public IEnumerator RotateTowardsDirection(Vector3 Target, Vector3 StartRot) //Ienum to rotate the ball to the desired direction
+    {
+
+        while (LerpFraction < 1) //while the lerp fraction is less than 1
+        {
+            yield return new WaitForEndOfFrame(); //wait until the end of the frame
+            LerpFraction += Time.deltaTime * RotSpeed; //adjust the lerp fraction
+            transform.eulerAngles = Vector3.Lerp(StartRot, Target, LerpFraction); //rotate the ball between start and target based on the lerp fraction
+            
+        }
+        LerpFraction = 0f; //reset the lerp fraction
+        _BallMoving = false; //change the bool as the ball is no longer moving
+        
+        
+    }
+
+    public IEnumerator MoveTowards(Vector3 StartPos, Vector3 Target) //Ienum to move the ball forwards
+    {
+        if (_BallMoving == false) //if the ball is not moving
+        {
+            _BallMoving = true; //set the bool to true as the ball is moving
+            Debug.Log("Move Towards"); //print to console
+
+
+            while (LerpFraction < 1) //while the lerp fraction is less than 1
+            {
+                yield return new WaitForEndOfFrame(); //wait unti the end of the frame
+                LerpFraction += Time.deltaTime * TravelSpeed; //adjust the lerp fraction
+                transform.localPosition = Vector3.Lerp(StartPos, Target, LerpFraction); //move the ball between start and target based on the lerp fraction
+            }
+            LerpFraction = 0f; //reset the lerp fraction
+            _BallMoving = false; //change the bool as the ball is not moving
+        }
+        
+
+    }
+}
