f675e191bddd248d743540ef899ba4970c9007db
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-21 12:20:27 +0100

    push before reworking the ballcontroller to work with the new grid

--- Assets/GridV3.cs
+++ Assets/GridV3.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridV3 : MonoBehaviour
 {
-
+    public List<GameObject> GridGameObjects = new List<GameObject>();
 
 
 
@@ -23,15 +23,36 @@ public class GridV3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < Length * Width; i++) //from video //https://www.youtube.com/watch?v=WJimYq2Tczc
-        {
-            Instantiate(prefab, new Vector3(X_Start + (X_Space * (i % Length)),transform.position.y, -Y_Start + (Y_Space * (i / Length))), Quaternion.identity);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CreateGrid();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            DeleteGrid();
+        }
+    }
+
+    public void CreateGrid()
+    {
+        for (int i = 0; i < Length * Width; i++) //from video //https://www.youtube.com/watch?v=WJimYq2Tczc
+        {
+            GridGameObjects.Add(Instantiate(prefab, new Vector3(X_Start + (X_Space * (i % Length)), transform.position.y, -Y_Start + (Y_Space * (i / Length))), Quaternion.identity));
+        }
+    }
+
+    public void DeleteGrid()
+    {
+        for (int i = 0; i < GridGameObjects.Count; i++)
+        {
+            Destroy(GridGameObjects[i]);
+        }
     }
 }
