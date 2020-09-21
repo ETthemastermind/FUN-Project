c41f14b820f26d1593f356d616a14bcf2977e370
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-21 11:45:21 +0100

    working on a grid system to work with the overlay

--- Assets/GridV2.cs
+++ Assets/GridV2.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class GridV2 : MonoBehaviour
 {
+    public float GridSizeX;
+    public float GridSizeY;
+
+    public float NumberOfCellsX;
+    public float NumberOfCellsY;
+
+    public float size;
     // Start is called before the first frame update
     void Start()
     {
+        GridSizeX = GetComponent<Collider>().bounds.size.x;
+        GridSizeY = GetComponent<Collider>().bounds.size.z;
+        for (int y = -4; y < GridSizeY; y++)
+        {
+            for (int i = 0; i < NumberOfCellsX; i++)
+            {
+
+                float Destination = (GridSizeX / NumberOfCellsX * i);
+                Debug.Log(Destination);
+                GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(Destination - 4.5f, transform.position.y, transform.position.z + 0.7f + y);
+            }
+        }
         
+        /*
+        for (int i = 0; i < NumberOfCellsY; i++)
+        {
+
+            float Destination = (GridSizeY / NumberOfCellsY) * i;
+            Debug.Log(Destination);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(transform.position.x, transform.position.y, Destination - 6.5f);
+        }
+        */
+        
+
     }
 
     // Update is called once per frame
---
+++ Assets/GridV3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridV3 : MonoBehaviour //https://www.youtube.com/watch?v=WJimYq2Tczc
+{
+    public float X_Start;
+    public float Y_Start;
+
+    public int Length;
+    public int Width;
+
+    public float X_Space;
+    public float Y_Space;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Length * Width; i++)
+        {
+            Instantiate(prefab, new Vector3(X_Start + (X_Space * (i % Length)),transform.position.y, -Y_Start + (Y_Space * (i / Length))), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
