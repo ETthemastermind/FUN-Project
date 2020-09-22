d7950ee17823a207b03f4248df78f36f89ce3c76
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-22 12:34:26 +0100

    moved button text to be under the buttons

--- Assets/Activity 1 - Ball and Balloons/Scripts/GridV3.cs
+++ Assets/Activity 1 - Ball and Balloons/Scripts/GridV3.cs
@@ -6,14 +6,14 @@ public class GridV3 : MonoBehaviour
 {
     public List<GameObject> GridGameObjects = new List<GameObject>();
 
-
+    
 
     [Header ("FromVid")] ////https://www.youtube.com/watch?v=WJimYq2Tczc
     public float X_Start; //default = -4.5
     public float Y_Start; // default = 6.8
 
-    public int Length; // default = 7
-    public int Width; //default = 10
+    public int Height; // default = 7
+    public int Width; //default = 11
 
     public float X_Space; //default = 1.5
     public float Y_Space; //default = 1.5
@@ -24,6 +24,8 @@ public class GridV3 : MonoBehaviour
     void Start()
     {
         CreateGrid();
+        Debug.Log((gameObject.GetComponent<Renderer>().bounds.size.x) / Height);
+        Debug.Log((gameObject.GetComponent<Renderer>().bounds.size.z) / Width);
     }
 
     // Update is called once per frame
@@ -38,13 +40,42 @@ public class GridV3 : MonoBehaviour
         {
             DeleteGrid();
         }
+
+
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+
+            RowUp();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            RowDown();
+
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ColumnUp();
+
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            ColumnDown();
+        }
+
+
+
+
+
+
     }
 
     public void CreateGrid()
     {
-        for (int i = 0; i < Length * Width; i++) //from video //https://www.youtube.com/watch?v=WJimYq2Tczc
+        for (int i = 0; i < Height * Width; i++) //from video //https://www.youtube.com/watch?v=WJimYq2Tczc
         {
-            GridGameObjects.Add(Instantiate(prefab, new Vector3(X_Start + (X_Space * (i % Length)), transform.position.y - 0.53f, -Y_Start + (Y_Space * (i / Length))), Quaternion.identity));
+            GridGameObjects.Add(Instantiate(prefab, new Vector3(X_Start + (X_Space * (i % Height)), transform.position.y - 0.53f, -Y_Start + (Y_Space * (i / Height))), Quaternion.identity));
         }
     }
 
@@ -54,5 +85,31 @@ public class GridV3 : MonoBehaviour
         {
             Destroy(GridGameObjects[i]);
         }
+    }
+
+    public void RowUp()
+    {
+        Height++;
+        X_Space -= 0.375f;
+        DeleteGrid();
+        CreateGrid();
+    }
+
+    public void RowDown()
+    {
+        Height--;
+        X_Space += 0.375f;
+        DeleteGrid();
+        CreateGrid();
+    }
+
+    public void ColumnUp()
+    {
+        Width++;
+    }
+
+    public void ColumnDown()
+    {
+        Width--;
     }
 }
