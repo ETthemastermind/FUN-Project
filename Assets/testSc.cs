2019ff3e3aa059601e460e7debf6d4b995e0bb88
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-22 17:58:36 +0100

    Final commit - In theory, have a method of doing a replay function

--- Assets/testSc.cs
+++ Assets/testSc.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testSc : MonoBehaviour
 {
+    public GameObject[] Buttons;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Replay());
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public IEnumerator Replay()
+    {
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            Buttons[i].GetComponent<Button>().onClick.Invoke();
+            yield return new WaitForSecondsRealtime(1f);
+        }
         
     }
 }
