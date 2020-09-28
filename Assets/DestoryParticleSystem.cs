fd47880e1c5365a40c1b327391fa9e44eaf38c1a
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-09-28 17:42:37 +0100

    self destroying particle script added

---
+++ Assets/DestoryParticleSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryParticleSystem : MonoBehaviour
+{
+    private ParticleSystem ps; //https://answers.unity.com/questions/219609/auto-destroying-particle-system.html
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ps.IsAlive() == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
