3bdbd3931a6f723d22c9b130d7cf67d0631b2772
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-10-02 10:57:57 +0100

    push before disabling a bunch of stuff

--- Assets/EyeTrack_POC.cs
+++ Assets/EyeTrack_POC.cs
@@ -9,20 +9,39 @@ public class EyeTrack_POC : MonoBehaviour
     public float MouseY;
 
     public List<float> TestList = new List<float>();
+    public Vector2[] GazeArray = new Vector2[60];
+    int i = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DataeryBoi", 0.0f, 1f / 60f);
+        InvokeRepeating("TestClear",1f,1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MouseX = Input.mousePosition.x;
-        MouseY = Input.mousePosition.y;
-        //TestList.Add((MouseX + MouseY));
+        
 
 
         
     }
+
+    public void DataeryBoi()
+    {
+        MouseX = Input.mousePosition.x;
+        MouseY = Input.mousePosition.y;
+        GazeArray[i] = new Vector2(MouseX, MouseY);
+        i++;
+        
+        
+    }
+
+    public void TestClear()
+    {
+        Array.Clear(GazeArray,0, GazeArray.Length);
+        i = 0;
+    }
+
 }
