b3d5548e649e03e2fea0232744879b7adbe8da03
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-10-06 12:45:25 +0100

    added a table underneath the board, replay telemetry script finds the right files from the right folder

--- Assets/ReplayingData/ReplayTelemetry.cs
+++ Assets/ReplayingData/ReplayTelemetry.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class ReplayTelemetry : MonoBehaviour
 {
     //public EyetrackingDataSave EDS;
     public EyetrackingDataSave[] EDS_Array;
     int i = 0;
+    private bool TelemetryActive;
+    
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Data", 0.0f, 1f / 60f);
+        TelemetryActive = gameObject.GetComponent<MasterTelemetrySystem>().TelemetryActive;
+        if (TelemetryActive == true)
+        {
+            CreateFile();
+            InvokeRepeating("Data", 0.0f, 1f / 60f);
+
+        }
+        
     }
 
     // Update is called once per frame
@@ -22,33 +34,64 @@ public class ReplayTelemetry : MonoBehaviour
 
     public void Data()
     {
-        EyetrackingDataSave EDS = new EyetrackingDataSave();
-        EDS.MouseX = Input.mousePosition.x;
-        EDS.MouseY = Input.mousePosition.y;
-        EDS.ButtonPressed = null;
-        EDS_Array[i] = EDS;
-        i++;
-        /*
-        EDS.MouseX = Input.mousePosition.x;
-        EDS.MouseY = Input.mousePosition.y;
+        if (i < 60)
+        {
+            EyetrackingDataSave EDS = new EyetrackingDataSave();
+            EDS.MouseX = Input.mousePosition.x;
+            EDS.MouseY = Input.mousePosition.y;
+            EDS.ButtonPressed = null;
+            EDS_Array[i] = EDS;
+            i++;
+        }
+        else
+        {
+            PushData();
+        }
         
-        //EDS_List.Add(EDS);
-        */
 
 
     }
     
     public void DatawithButtonPress(GameObject ButtonPress)
     {
-        EyetrackingDataSave EDS = new EyetrackingDataSave();
-        EDS.MouseX = Input.mousePosition.x;
-        EDS.MouseY = Input.mousePosition.y;
-        EDS.ButtonPressed = ButtonPress;
-        EDS_Array[i] = EDS;
-        i++;
+        if (i < 60)
+        {
+            EyetrackingDataSave EDS = new EyetrackingDataSave();
+            EDS.MouseX = Input.mousePosition.x;
+            EDS.MouseY = Input.mousePosition.y;
+            EDS.ButtonPressed = ButtonPress;
+            EDS_Array[i] = EDS;
+            i++;
+        }
+        else
+        {
+            PushData();
+        }
+        
 
 
 
+
+    }
+
+    public void PushData()
+    {
+        if (TelemetryActive == true)
+        {
+            Debug.Log("Data Pushed");
+            Array.Clear(EDS_Array, 0, EDS_Array.Length);
+        }
+
+    }
+
+    public void CreateFile()
+    {
+        
+        string ID = gameObject.GetComponent<MasterTelemetrySystem>().ID;
+        string FileName = "/" + ID + ".FUNREPLAY";
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Create(Application.streamingAssetsPath + "/Replay Files" + FileName);
+        fs.Close();
 
     }
     
---
+++ Assets/ViewReplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class ViewReplay : MonoBehaviour //https://answers.unity.com/questions/43780/getting-list-of-files-from-specified-folder.html
+{
+    public bool ReplayFile;
+    public FileInfo[] Files;
+    public List<string> list;
+    public enum MyEnum
+    {
+
+
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath + "/Replay FIles");
+        Files = dir.GetFiles("*.FUNREPLAY");
+        
+        /*
+        foreach (FileInfo f in Files)
+        {
+            list.Add(f.FullName);
+        }
+        */
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
